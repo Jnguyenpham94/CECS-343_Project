e4dfc9ae37243b438ea5e1a2d9dbea5369255042
Nguyen Doan <npdoan1996@gmail.com>
2020-06-18 22:30:29 -0700

    commented obj.close() to get thing work

--- LoginLogout.java
+++ LoginLogout.java
@@ -1,65 +1,59 @@
 import java.util.Scanner;
-public class LoginLogout{
 
-    public LoginLogout()
-    {
+public class LoginLogout {
+
+    public LoginLogout() {
         this.userID = "JohnN";
         this.password = "1234";
     }
 
-    public void login()
-    {
+    public void login() {
         Scanner obj = new Scanner(System.in);
         promptUsername();
         String user = obj.nextLine();
         promptPassword();
         String pass = obj.nextLine();
         boolean entry = false;
-        while(entry == false){
-            if(user.equals(userID) && password.equals(pass)){
+        while (entry == false) {
+            if (user.equals(userID) && password.equals(pass)) {
                 entry = true;
-            }
-            else{
+            } else {
                 invalidLogin();
                 entry = true;
             }
         }
-        obj.close();
+        // obj.close(); have to comment this line to get thing works
         UserInterface ui = new UserInterface();
         ui.Menu();
+        ui.userInput();
     }
 
-    public void promptUsername()
-    {
+    public void promptUsername() {
         System.out.print("Enter username: ");
     }
 
-    public void promptPassword()
-    {
+    public void promptPassword() {
         System.out.print("Enter password: ");
     }
 
-    public void invalidLogin()
-    {
+    public void invalidLogin() {
         System.out.println("Login Failed \nDO you want to try again y/n?");
         Scanner obj = new Scanner(System.in);
         String temp = obj.nextLine();
-        if(temp.equalsIgnoreCase("y")){
+        if (temp.equalsIgnoreCase("y")) {
             login();
-        }
-        else{
+        } else {
             obj.close();
             logout();
         }
     }
 
-    public void logout()
-    {
+    public void logout() {
         System.out.println("Logging out.");
         System.exit(0);
     }
 
     private String userID;
     private String password;
-    
+
 }
